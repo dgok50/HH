1c0cb4a57f0e0a6cd4577ebe358272b19dba3db5
Chizh <Chizha2@gmail.com>
2018-03-16 00:25:11 +0300

    Исправление пагинации в train и начало HH

--- HH/HH.py
+++ HH/HH.py
@@ -1,1 +1,11 @@
+import requests # для HTTP запросов
+url = 'https://api.hh.ru/vacancies' # ссылка для работы с вакансиями (из API)
 
+for i in range(5): # по страницам сайта
+    par = {'text': 'data analytics','per_page':'20', 'page':i} # параметры запроса
+    r = requests.get(url, par) # выполнение запроса
+    e = r.json() # декодирование json
+    y = e['items'] # информация о вакансиях
+
+    for i in y: # переберает вакансии
+        print("job")
